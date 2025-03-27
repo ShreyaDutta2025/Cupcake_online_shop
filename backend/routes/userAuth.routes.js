cf262e4b158c4bf32d9ae1d367ef8343a5b9cfb2
ShreyaDutta2025 <duttashreya349@gmail.com>
2025-03-27 15:00:46 +0530

    Update userAuth.routes.js

--- backend/routes/userAuth.routes.js
+++ backend/routes/userAuth.routes.js
@@ -1,15 +1,18 @@
 const express = require('express');
+// Importing user authentication controller functions
 const { register, login,getUserProfile ,update,deleteUser, logout} = require('../controller/userAuth.controller');
+
+// Middleware for authentication and token verification
 const authMiddleware = require('../middleware/authMiddleware')
  
 const router = express.Router();
  
-router.post('/register', register);
-router.post('/login', login);
-router.get('/profile/:id', getUserProfile); 
-router.put('/update/:id',update);
-router.delete('/delete', deleteUser);
-router.post('/logout', authMiddleware,logout)
+router.post('/register', register); // Route for user registration
+router.post('/login', login); // Route for user login
+router.get('/profile/:id', getUserProfile); // Route to get user profile by ID
+router.put('/update/:id',update);   // Route to update user details
+router.delete('/delete', deleteUser); // Route to delete a user account
+router.post('/logout', authMiddleware,logout);  // Route to log out the user
  
 module.exports = router;
  
