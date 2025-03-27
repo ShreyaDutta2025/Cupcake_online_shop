a9e81742dc50fd0594bc4311a6096c3c5259ab0d
ShreyaDutta2025 <duttashreya349@gmail.com>
2025-03-27 14:57:29 +0530

    Update category.routes.js

--- backend/routes/category.routes.js
+++ backend/routes/category.routes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
+// Importing category controller functions
 const { getAllCategories, addCategory } = require('../controller/category.controller');
  
-router.post('/addcategory', addCategory)
-router.get('/', getAllCategories);
+router.post('/addcategory', addCategory); // Route to add a new category
+router.get('/', getAllCategories); // Route to get all categories
  
 module.exports = router;
