a9d32659506863b2c171cfceed71717e35625d52
ShreyaDutta2025 <duttashreya349@gmail.com>
2025-03-27 14:51:51 +0530

    Update cake.routes.js

--- backend/routes/cake.routes.js
+++ backend/routes/cake.routes.js
@@ -3,12 +3,12 @@ const router = express.Router();
 const upload = require("../middleware/upload.middleware");
 const { addCake, getCakes, searchCakes, viewCake, getCakesByCategory } = require("../controller/cake.controller");
  
-
+//route for adding cakes
 router.post("/add", upload.single("image"), addCake);
 
 
-router.get("/all", getCakes);
-router.get("/search", searchCakes)
-router.get("/:cakeId", viewCake)
-router.get('/category/:categoryName',getCakesByCategory)
+router.get("/all", getCakes);//route for get all the cakes
+router.get("/search", searchCakes); //route for searching cake
+router.get("/:cakeId", viewCake);  // Route to view a specific cake by its ID 
+router.get('/category/:categoryName',getCakesByCategory);  // Route to get cakes by category
 module.exports = router;
