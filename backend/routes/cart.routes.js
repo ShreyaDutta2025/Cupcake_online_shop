2ebf9715d531c20fd7dc8b15a0117638b0c2f320
ShreyaDutta2025 <duttashreya349@gmail.com>
2025-03-27 14:55:13 +0530

    Update cart.routes.js

--- backend/routes/cart.routes.js
+++ backend/routes/cart.routes.js
@@ -1,16 +1,17 @@
 const express = require("express");
+// Importing cart controller functions
 const { addToCart, getCartWithTotal,updateCart, removeFromCart } = require("../controller/cart.controller");
  
 const router = express.Router();
  
 
-router.post("/add", addToCart);
+router.post("/add", addToCart); // Route to add an item to the cart
  
 
-router.get("/:userId", getCartWithTotal);
+router.get("/:userId", getCartWithTotal);  // Route to get the cart details with the total amount
 
-router.put('/update', updateCart)
+router.put('/update', updateCart);  // Route to update an item in the cart
 
-router.delete("/remove/:cartItemId", removeFromCart);
+router.delete("/remove/:cartItemId", removeFromCart);  // Route to remove an item from the cart
  
 module.exports = router;
