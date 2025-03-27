1edfe10c5929c9a63e85bf4b874e2258b40e929c
ShreyaDutta2025 <duttashreya349@gmail.com>
2025-03-27 15:03:17 +0530

    Update wishlist.route.js

--- backend/routes/wishlist.route.js
+++ backend/routes/wishlist.route.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
+// Importing wishlist controller functions
 const wishListController = require('../controller/wishlist.controller');
 
+// Route to add an item to the wishlist
 router.post('/add', wishListController.addToWishList);
 
+// Route to get the user's wishlist by user ID
 router.get('/getwish/:userId', wishListController.getWishList);
 
 module.exports = router;
